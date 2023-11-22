f4fd2b63fd075ef6f057d40b98b20a863658fafa
pisalcoding <pisalcoding@example.com>
2023-11-22 14:27:03 +0700

    Update publishing script

--- alerter/build.gradle.kts
+++ alerter/build.gradle.kts
@@ -47,7 +47,7 @@ configure<PublishingExtension> {
         register<MavenPublication>("release") {
             groupId = "com.github.utngypisal"
             artifactId = "alerter"
-            version = "1.0.0"
+            version = "v1.0.0"
 
             afterEvaluate {
                 from(components["release"])
